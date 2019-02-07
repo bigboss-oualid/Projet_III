4cf85338126fb32bb49843de19a68cfd207e71b8
BigBoss <oualid.boulatar@gmail.com>
2019-02-07 02:05:23 +0100

    feat(System): add script Controller.php
    abstract class that will be extends by all classes controllers

---
+++ vendor/System/Controller.php
@@ -0,0 +1,35 @@
+<?php 
+
+namespace System;
+
+abstract class Controller
+{
+	/**
+	 * Application Object
+	 * 
+	 * @var \System\Application
+	 */
+	protected $app;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param \System\Application $app 
+	 */
+	public function __construct(Application $app)
+	{
+		$this->app = $app;
+	}
+
+	/**
+	 * Call shared Application bjects dynamically
+	 *
+	 * ´@param string  $key
+	 * @return mixed
+	 */
+	public function __get(string $key)
+	{
+		return $this->app->get($key);
+	}
+
+} 
