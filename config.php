6b06c74423de8cf152a5791c335259fc7261a8bf
BigBoss <oualid.boulatar@gmail.com>
2019-03-22 01:17:29 +0100

    chore: update config.php

--- config.php
+++ config.php
@@ -1,13 +1,15 @@
 <?php 
 
 return [
+//Data for database connexion
 	'db' =>[
 		'server'   =>  'localhost',
-		'dbname'   =>  'blog-try',
+		'dbname'   =>  'blog_ecrivain',
 		'dbuser'   =>  'root',
 		'dbpass'   =>  '',
 	],
 
+//Allowed image extensions
 	'image_extensions'   =>  [
 					'gif',
 					'jpg',
@@ -17,4 +19,15 @@ return [
 					'raw',
 					'bmp',
 	],
+
+//Setting for the site
+	'site_settings'   =>  [
+					'site_name',
+					'site_email',
+					'site_status',
+					'comments_status',
+					'site_close_msg',
+					'episodes_in_home',
+	],
 ];
+
